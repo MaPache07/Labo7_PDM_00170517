e95d3ef2287eff1b1c0d03476bfcc479169bc398
MaPache07 <00170517@uca.edu.sv>
2019-05-17 15:41:53 -0600

    Supuestamente funciona :v

--- app/src/main/java/com/example/labo7_00170517/MainActivity.kt
+++ app/src/main/java/com/example/labo7_00170517/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.labo7_00170517
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.labo7_00170517.adapters.ReposAdapter
+import com.example.labo7_00170517.database.entities.GithubRepo
+import com.example.labo7_00170517.database.viewmodels.GithubRepoViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ReposAdapter
+    lateinit var viewModel : GithubRepoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bind()
+    }
+
+    fun bind(){
+        adapter = ReposAdapter(ArrayList())
+        //viewModel = ViewModelProviders.of(this).get(GithubRepoViewModel::class.java)
+        recycler.apply {
+            adapter = this@MainActivity.adapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+
+        btn_add.setOnClickListener {
+            viewModel.insert(GithubRepo(et_repo_name.text.toString()))
+        }
     }
 }
